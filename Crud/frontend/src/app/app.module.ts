65f4775f6c2890d7020e0f6822250ea24653a70e
Gustavo Ferreira Wanderley <mestreguga24@gmail.com>
2022-04-25 20:16:44 -0300

    Quarto commit
    - Adição do view que será a parte de exibição dos conteúdos das páginas
    - Adição do sistema de rotas que está ligado entre a Home e a parte do CRUD

--- Crud/frontend/src/app/app.module.ts
+++ Crud/frontend/src/app/app.module.ts
@@ -11,7 +11,11 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import {MatSidenavModule } from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list'
+import {MatCardModule } from '@angular/material/card'
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+
 
 @NgModule({
   declarations: [
@@ -19,6 +23,8 @@ import { MatListModule } from '@angular/material/list'
     HeaderComponent,
     FooterComponent,
     NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
     
   ],
   imports: [
@@ -27,7 +33,8 @@ import { MatListModule } from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
    
   ],
   providers: [],
